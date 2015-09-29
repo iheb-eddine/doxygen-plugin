a70bba475deb0919646baf451158048c6e280d8e
Iheb Eddine BADRANI <ihebeddine@MacBook-Pro-de-itatchi.local>
2015-09-29 21:16:59 +0100

    Initial Commit

---
+++ Doxygen Plugin/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Doxygen Plugin
+//
+//  Created by Iheb Eddine BADRANI on 29/09/2015.
+//  Copyright (c) 2015 Iheb Eddine BADRANI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
